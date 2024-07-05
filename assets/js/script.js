0d4141338be2b58851ab7b7ff63557ab9dda505f
german-rs <18427568+german-rs@users.noreply.github.com>
2024-07-05 17:46:14 -0400

    Carrusel, tooltip, jquey ingredientes y preparación toggle cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,25 @@
     Desafío: Ricomida. 
 */
 
+$(document).ready(function () {
+
+    //2.- Tooltip.
+    $('[data-bs-toggle="tooltip"]').tooltip();
+
+
+    //3.- Evento.
+    $("#enviarCorreo").click(function(){
+        alert("El correo fue enviado correctamente...")
+    });
+
+    //4.- Selectores de etiqueta.
+    $("h3").on("dblclick", function() {
+        $(this).css("color", "red");
+    });
+
+    //5.- Selectores de clase.
+    $(".card-body h5").on('click', function () {
+        $('.card-body p').toggle('slow');
+    });
+
+});
